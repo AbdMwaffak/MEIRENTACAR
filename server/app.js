0c22bf42258a6bcef9bc6f490d8604b2b8eb47c6
Abd Abouhawa <abdabouhawa@gmail.com>
2024-05-03 18:18:42 +0300

    some permession updated

--- server/app.js
+++ server/app.js
@@ -17,7 +17,19 @@ const settingRoutes = require('./routes/settingRoutes');
 
 dotenv.config();
 const app = express();
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin');
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '../client/build/index.html')));
 app.set('trust proxy', 1);
 ////
 const limiter = rateLimit({
@@ -31,7 +43,6 @@ const limiter = rateLimit({
 app.use('/', limiter);
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
